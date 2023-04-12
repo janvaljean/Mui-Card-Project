397e7758b8e6dd246223c569b511967f547e9d43
janvaljean <kurtuldu@gmx.de>
2023-04-12 19:30:16 +0200

    Appbar done

---
+++ mui-card-project/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <AppBar>
+
+        <Toolbar sx={{fontSize:30}}> Card Project</Toolbar>
+      
+      </AppBar>
+    </div>
+  )
+}
+
+export default App
