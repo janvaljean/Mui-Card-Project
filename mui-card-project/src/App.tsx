6788c4f105a052bca085238a8bc11b1a55b28005
janvaljean <kurtuldu@gmx.de>
2023-04-15 14:38:39 +0200

    course card added with mui

--- mui-card-project/src/App.tsx
+++ mui-card-project/src/App.tsx
@@ -1,19 +1,55 @@
-
-import './App.css'
+import "./App.css";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Course from "./components/Course";
+import Angular from "./images/angular.jpg";
+import Bootstrap from "./images/bootstrap5.png";
+import Csharp from "./images/ccsharp.png";
+import CompleteWeb from "./images/kompleweb.jpg";
+
 function App() {
- 
-
   return (
-    <div className="App">
+    <div>
       <AppBar>
-
-        <Toolbar sx={{fontSize:30}}> Card Project</Toolbar>
-      
+        <Toolbar sx={{ fontSize: 30 }}> Card Project</Toolbar>
       </AppBar>
+
+      <Container maxWidth="lg">
+        <Grid container spacing={2} sx={{ marginTop: 10 }}>
+          <Grid item xs={12} md={6} lg={3}>
+            <Course
+              image={Angular}
+              title="Angular"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut a tempore voluptatum consectetur eius amet autem! Illum voluptate, optio, eius fugiat, dolores placeat sequi animi consequatur laborum totam debitis earumEx amet nemo necessitatibus tempore autem perspiciatis vel nihil, deleniti, magnam dolorum error id reprehenderit quisquam enim odit unde dolorem numquam assumenda molestiae quaerat debitis cum! Saepe deserunt molestias autem."
+            />
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Course
+              image={Bootstrap}
+              title="Bootstrap"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut a tempore voluptatum consectetur eius amet autem! Illum voluptate, optio, eius fugiat, dolores placeat sequi animi consequatur laborum totam debitis earumEx amet nemo necessitatibus tempore autem perspiciatis vel nihil, deleniti, magnam dolorum error id reprehenderit quisquam enim odit unde dolorem numquam assumenda molestiae quaerat debitis cum! Saepe deserunt molestias autem."
+            />
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Course
+              image={Csharp}
+              title="Csharp"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut a tempore voluptatum consectetur eius amet autem! Illum voluptate, optio, eius fugiat, dolores placeat sequi animi consequatur laborum totam debitis earumEx amet nemo necessitatibus tempore autem perspiciatis vel nihil, deleniti, magnam dolorum error id reprehenderit quisquam enim odit unde dolorem numquam assumenda molestiae quaerat debitis cum! Saepe deserunt molestias autem."
+            />
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Course
+              image={CompleteWeb}
+              title="CompleteWeb"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut a tempore voluptatum consectetur eius amet autem! Illum voluptate, optio, eius fugiat, dolores placeat sequi animi consequatur laborum totam debitis earumEx amet nemo necessitatibus tempore autem perspiciatis vel nihil, deleniti, magnam dolorum error id reprehenderit quisquam enim odit unde dolorem numquam assumenda molestiae quaerat debitis cum! Saepe deserunt molestias autem."
+            />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- mui-card-project/src/components/Course.tsx
+++ mui-card-project/src/components/Course.tsx
@@ -1,11 +1,29 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+type Data = {
+  title: string;
+  description: string;
+  image: any;
+};
 
-
-const Course = () => {
+const Course = (props : Data) => {
   return (
     <div>
-      
+      <Card >
+        <CardMedia sx={{ height: 140 }} image={props.image} title={props.title} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.description}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default Course
+export default Course;
